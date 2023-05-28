3885b5a5f298548686aad216ffe1dbd4db895992
tornikee <tornikexoxiashvili@gmail.com>
2023-05-28 14:07:25 +0400

    change

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -9,20 +9,19 @@ public class CatchEmAll {
     static Exception exception = new FileNotFoundException();
 
     public static void riskyMethod() throws Exception {
-        try {
             throw exception;
-        } catch (IOException e) {
-            if (e instanceof FileNotFoundException) {
-                throw new IllegalArgumentException("Resource is missing", e);
-            } else {
-                throw new IllegalArgumentException("Resource error", e);
-            }
-        } catch (ArithmeticException | NumberFormatException e) {
-            System.err.println("Exception occurred: " + e.getMessage());
-        }
+
     }
 
     public static void main(String[] args) throws Exception {
+        try {
             riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource error",e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
